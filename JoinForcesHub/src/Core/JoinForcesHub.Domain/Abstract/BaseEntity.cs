7d70a3033cb183a7e54b89d420de54f8413e3f50
Abdullah Balıkçı <abdullah.balikci20@ogr.atauni.edu.tr>
2023-10-24 11:40:00 +0300

    user entity and base entity have been created.

---
+++ JoinForcesHub/src/Core/JoinForcesHub.Domain/Abstract/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace JoinForcesHub.Domain.Abstract;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public DateTime  CreationDate  { get; set; }
+    public string CreatedByUserName { get; set; }
+    public DateTime? UpdatedDate { get; set; }
+    public string UpdatedByUserName { get; set; }
+    public bool  IsActive  { get; set; }
+    public bool  IsDeleted  { get; set; }
+}
---
+++ JoinForcesHub/src/Core/JoinForcesHub.Domain/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using JoinForcesHub.Domain.Abstract;
+
+namespace JoinForcesHub.Domain.Entities;
+
+public class User :BaseEntity
+{
+    public string FirstName { get; set; }
+    public string SurName { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string About { get; set; }
+    public string PasswordSalt { get; set; }
+    public string PasswordHash { get; set; }
+    public string PhotoPath { get; set; }
+    public string GithubLink { get; set; }
+    public string LinkedlnLink { get; set; }
+    public string StackOverFlowLink { get; set; }
+    public string Location { get; set; }
+    public string EducationStatus { get; set; }
+    public string JobStatus { get; set; }
+    public string IsProfileVisible { get; set; }
+    public string CoverPhoto { get; set; }
+    public bool IsPremium { get; set; }
+}
