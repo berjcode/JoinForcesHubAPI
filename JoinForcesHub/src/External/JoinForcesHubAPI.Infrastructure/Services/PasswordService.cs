8271feb9e14079617801ef02f1e040026d064793
Abdullah Balıkçı <abdullah.balikci20@ogr.atauni.edu.tr>
2023-11-14 11:26:13 +0300

    user password, hashing updated from sha-256 to sha-512

--- JoinForcesHub/src/External/JoinForcesHubAPI.Infrastructure/Services/PasswordService.cs
+++ JoinForcesHub/src/External/JoinForcesHubAPI.Infrastructure/Services/PasswordService.cs
@@ -1,7 +1,8 @@
-﻿using Azure.Core;
+﻿using System.Text;
 using System.Security.Cryptography;
 using JoinForcesHubAPI.Application.Common.Interfaces.Services;
 using JoinForcesHubAPI.Application.Contracts.UserAuthentication;
+
 
 namespace JoinForcesHubAPI.Infrastructure.Services;
 
@@ -10,16 +11,17 @@ public class PasswordService : IPasswordService
     public PasswordDto HashPassword(string password)
     {
         string generateSalt = GenerateSalt();
-        string newHashPassword = BCryptHashPasswordAsync(password, generateSalt);
-
+        string newHashPassword = SHA512HashPasswordAsync(password, generateSalt);
         PasswordDto passwordDto = new(PasswordHash: newHashPassword, Salt: generateSalt);
-        return passwordDto;
 
+        return passwordDto;
     }
 
     public bool VerifyPassword(string password, string salt, string hashedPassword)
     {
-        return BCrypt.Net.BCrypt.Verify(password + salt, hashedPassword);
+        string newHashedPassword = SHA512HashPasswordAsync(password, salt);
+
+        return string.Equals(newHashedPassword, hashedPassword, StringComparison.Ordinal);
     }
     private string BCryptHashPasswordAsync(string password, string salt)
     {
@@ -34,5 +36,15 @@ public class PasswordService : IPasswordService
             random.GetBytes(randomBytes);
 
         return Convert.ToBase64String(randomBytes);
+    }
+    private string SHA512HashPasswordAsync(string password, string salt)
+    {
+        using (SHA512Managed sha512 = new SHA512Managed())
+        {
+            byte[] data = Encoding.UTF8.GetBytes(password + salt);
+            byte[] hash = sha512.ComputeHash(data);
+
+            return Convert.ToBase64String(hash);
+        }
     }
 }
